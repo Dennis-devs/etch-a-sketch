bc7daa22d2715fa30c9f39c3904eaf927d3e23cd
Dennis-devs <dennis.ogoll@gmail.com>
2022-06-18 15:34:01 +0300

    set up a hover effect
    set up a hover effect by changing the divs background color using javascript

--- sketch.js
+++ sketch.js
@@ -1,20 +1,43 @@
 function createDivs(){
    let bigdiv = document.querySelector('#big');
-   let cols = 16;
-   let rows = 16;
+   let smalldiv;
+   let cols = 64;
+   let rows = 64;
    let gridcount = cols*rows;
    for (let i = 0; i <= gridcount; i++) {
       if(bigdiv.childElementCount < gridcount){
-      let smalldiv = document.createElement('div')
+      smalldiv = document.createElement('div')
       smalldiv.className = 'grids'
-      bigdiv.appendChild(smalldiv)
+      bigdiv.appendChild(smalldiv);
       }
    }
-  
+   /*let myRules = document.styleSheets[0];
+   let newColor;
+   for (let i = 0; i < myRules.cssRules.length; i++) {
+      if(myRules.cssRules[i].selectorText === '.grids'){
+         newColor = myRules.cssRules[i];
+      }       
+   }
 
+   function random() {
+      const num = Math.min(0, 255)
+      console.log(num)
+      return num;
+    }
+    function newcol() {
+      console.log ( 'rgb(' + random() + ', ' + random() + ', ' + random() +  ')');
+      return 'rgb(' + random() + ', ' + random() + ', ' + random() +  ')';
+    }*/
+
+    let smalldivs = document.querySelectorAll('.grids')
+   smalldivs.forEach(item => {item.addEventListener('mousemove', function(){
+      item.setAttribute('style', 'background: black;');
+   })})
+   
+      
 }
  createDivs()
-
+ 
 
 
 
